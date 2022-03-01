b168800c39f662065d1336c7e0e3c296b69d51ff
André <andre.gmaps@gmail.com>
2022-03-01 12:56:41 -0300

    gerando testes de unidade

--- src/main/java/br/com/alura/leilao/service/GeradorDePagamento.java
+++ src/main/java/br/com/alura/leilao/service/GeradorDePagamento.java
@@ -1,24 +1,36 @@
 package br.com.alura.leilao.service;
 
-import java.time.LocalDate;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import br.com.alura.leilao.dao.PagamentoDao;
 import br.com.alura.leilao.model.Lance;
 import br.com.alura.leilao.model.Pagamento;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
 
 @Service
 public class GeradorDePagamento {
 
-	@Autowired
-	private PagamentoDao pagamentos;
+    @Autowired
+    private PagamentoDao pagamentos;
 
-	public void gerarPagamento(Lance lanceVencedor) {
-		LocalDate vencimento = LocalDate.now().plusDays(1);
-		Pagamento pagamento = new Pagamento(lanceVencedor, vencimento);
-		this.pagamentos.salvar(pagamento);
-	}
+    public void gerarPagamento(Lance lanceVencedor, LocalDate vencimento) {
+        Pagamento pagamento = new Pagamento(lanceVencedor, proximoDiaUtil(vencimento));
+        this.pagamentos.salvar(pagamento);
+    }
+
+    private LocalDate proximoDiaUtil(LocalDate data) {
+        DayOfWeek diaDaSemana = data.getDayOfWeek();
+
+        switch (diaDaSemana) {
+            case FRIDAY:
+                return data = data.plusDays(3);
+            case SATURDAY:
+                return data = data.plusDays(2);
+            default:
+                return data = data.plusDays(1);
+        }
+    }
 
 }
--- src/test/java/br/com/alura/leilao/service/GeradorDePagamentoTest.java
+++ src/test/java/br/com/alura/leilao/service/GeradorDePagamentoTest.java
@@ -6,10 +6,12 @@ import br.com.alura.leilao.model.Lance;
 import br.com.alura.leilao.model.Leilao;
 import br.com.alura.leilao.model.Pagamento;
 import br.com.alura.leilao.model.Usuario;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.*;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.math.BigDecimal;
@@ -17,7 +19,8 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -42,13 +45,16 @@ class GeradorDePagamentoTest {
     FinalizarLeilaoService finalizarLeilaoService;
 
     @Test
-    void deveGerarPagamentoParaVencedorDoLeilao() {
+    void deveGerarPagamentoParaVencedorDoLeilaoParaOproximoDiaUtilCasoOvencimentoSejaDiaUtil() {
         List<Leilao> leiloes = leiloes();
 
         when(leilaoDao.buscarLeiloesExpirados()).thenReturn(leiloes);
+
         finalizarLeilaoService.finalizarLeiloesExpirados();
+
         Lance lanceVencedor = leiloes.get(0).getLanceVencedor();
-        geradorDePagamento.gerarPagamento(lanceVencedor);
+
+        geradorDePagamento.gerarPagamento(lanceVencedor, LocalDate.of(2022, 3, 1));
 
         verify(pagamentoDao, times(1)).salvar(captor.capture());
 
@@ -56,7 +62,51 @@ class GeradorDePagamentoTest {
         assertEquals(lanceVencedor.getLeilao(), pagamento.getLeilao());
         assertEquals(lanceVencedor.getUsuario(), pagamento.getUsuario());
         assertEquals(lanceVencedor.getValor(), pagamento.getValor());
-        assertEquals(LocalDate.now().plusDays(1), pagamento.getVencimento());
+        assertEquals(LocalDate.of(2022, 3, 2), pagamento.getVencimento());
+        assertFalse(pagamento.getPago());
+    }
+
+    @Test
+    void deveGerarPagamentoParaVencedorDoLeilaoParaOproximoDiaUtilCasoOvencimentoSejaSextaFeira() {
+        List<Leilao> leiloes = leiloes();
+
+        when(leilaoDao.buscarLeiloesExpirados()).thenReturn(leiloes);
+
+        finalizarLeilaoService.finalizarLeiloesExpirados();
+
+        Lance lanceVencedor = leiloes.get(0).getLanceVencedor();
+
+        geradorDePagamento.gerarPagamento(lanceVencedor, LocalDate.of(2022, 2, 25));
+
+        verify(pagamentoDao, times(1)).salvar(captor.capture());
+
+        Pagamento pagamento = captor.getValue();
+        assertEquals(lanceVencedor.getLeilao(), pagamento.getLeilao());
+        assertEquals(lanceVencedor.getUsuario(), pagamento.getUsuario());
+        assertEquals(lanceVencedor.getValor(), pagamento.getValor());
+        assertEquals(LocalDate.of(2022, 2, 28), pagamento.getVencimento());
+        assertFalse(pagamento.getPago());
+    }
+
+    @Test
+    void deveGerarPagamentoParaVencedorDoLeilaoParaOproximoDiaUtilCasoOvencimentoSejaSabado() {
+        List<Leilao> leiloes = leiloes();
+
+        when(leilaoDao.buscarLeiloesExpirados()).thenReturn(leiloes);
+
+        finalizarLeilaoService.finalizarLeiloesExpirados();
+
+        Lance lanceVencedor = leiloes.get(0).getLanceVencedor();
+
+        geradorDePagamento.gerarPagamento(lanceVencedor, LocalDate.of(2022, 2, 19));
+
+        verify(pagamentoDao, times(1)).salvar(captor.capture());
+
+        Pagamento pagamento = captor.getValue();
+        assertEquals(lanceVencedor.getLeilao(), pagamento.getLeilao());
+        assertEquals(lanceVencedor.getUsuario(), pagamento.getUsuario());
+        assertEquals(lanceVencedor.getValor(), pagamento.getValor());
+        assertEquals(LocalDate.of(2022, 2, 21), pagamento.getVencimento());
         assertFalse(pagamento.getPago());
     }
 
@@ -66,7 +116,7 @@ class GeradorDePagamentoTest {
         Leilao leilao = new Leilao("Celular", new BigDecimal("500"), new Usuario("Fulano"));
 
         Lance primeiro = new Lance(new Usuario("Beltrano"), new BigDecimal("600"));
-        Lance segundo =new Lance(new Usuario("Ciclano"), new BigDecimal("900"));
+        Lance segundo = new Lance(new Usuario("Ciclano"), new BigDecimal("900"));
 
         leilao.propoe(primeiro);
         leilao.propoe(segundo);
